23bf713eb4b2057d30156c77160af7a6a2397999
teezyfortune <gabteezy14@gmail.com>
2019-11-14 17:57:00 +0100

    bg[bug-fix]: fix failing test

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ TravisCi
 [![Build Status](https://travis-ci.com/teezyfortune/TeamWork-Backend-API.svg?branch=develop)](https://travis-ci.com/teezyfortune/TeamWork-Backend-API)
 
 [![Coverage Status](https://coveralls.io/repos/github/teezyfortune/TeamWork-Backend-API/badge.svg?branch=develop)](https://coveralls.io/github/teezyfortune/TeamWork-Backend-API?branch=develop)
+
+Teamwork is an internal social network for employees of an organization. The goal of this application is to facilitate more interaction between colleagues and promote team bonding
--- src/modules/articles/article.spec.js
+++ src/modules/articles/article.spec.js
@@ -8,47 +8,46 @@ import * as mock from '../../services/articles/__mocks__';
 const { expect } = chai;
 chai.use(chaHttp);
 let userToken;
-describe('Test Suite that create articles', () => {
-  describe('Authentication: Signin User', () => {
-    it('ARTICLE', (done) => {
-      chai
-        .request(app)
-        .post(mock.baseLogin)
-        .send(mock.signIn)
-        .end((err, response) => {
-          userToken = response.body.data;
-          if (err) done(err);
-          expect(response.statusCode).to.equal(200);
-          done();
-        });
-    });
 
-    it('It should create new article', (done) => {
-      chai
-        .request(app)
-        .post(mock.baseUrl)
-        .set('authorization', `Bearer ${userToken.token}`)
-        .send(mock.article)
-        .end((err, response) => {
-          if (err) done(err);
-          expect(response.statusCode).to.equal(201);
-          expect(response.body).to.contains({ status: 'success' });
-          done();
-        });
-    });
-    it('It should respond with field can not be empty', (done) => {
-      chai
-        .request(app)
-        .post(mock.baseUrl)
-        .set('authorization', `Bearer ${userToken.token}`)
-        .send(mock.emptySpace)
-        .end((err, response) => {
-          if (err) done(err);
-          expect(response.statusCode).to.equal(422);
-          expect(response.body).to.contains({ status: 'error' });
-          done();
-        });
-    });
+describe('Authentication: Signin User', () => {
+  it('ARTICLE', (done) => {
+    chai
+      .request(app)
+      .post(mock.baseLogin)
+      .send(mock.signIn)
+      .end((err, response) => {
+        userToken = response.body.data;
+        if (err) done(err);
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+  });
+
+  it('It should create new article', (done) => {
+    chai
+      .request(app)
+      .post(mock.baseUrl)
+      .set('authorization', `Bearer ${userToken.token}`)
+      .send(mock.article)
+      .end((err, response) => {
+        if (err) done(err);
+        expect(response.statusCode).to.equal(201);
+        expect(response.body).to.contains({ status: 'success' });
+        done();
+      });
+  });
+  it('It should respond with field can not be empty', (done) => {
+    chai
+      .request(app)
+      .post(mock.baseUrl)
+      .set('authorization', `Bearer ${userToken.token}`)
+      .send(mock.emptySpace)
+      .end((err, response) => {
+        if (err) done(err);
+        expect(response.statusCode).to.equal(422);
+        expect(response.body).to.contains({ status: 'error' });
+        done();
+      });
   });
 });
 
