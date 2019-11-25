0b84142ccc0f6c2eca8ae4a137d59449330963b7
teezyfortune <gabteezy14@gmail.com>
2019-11-25 01:06:43 +0100

    bg(user-profile)

--- src/modules/authentication/login.js
+++ src/modules/authentication/login.js
@@ -6,35 +6,6 @@ import conn from '../../database';
 
 const loginUser = async (request, response) => {
   try {
-<<<<<<< HEAD
-    const { username, password } = await request.body;
-    const findUser = await getOneUserByEmail(username);
-    if (findUser.rowCount !== 0) {
-      const { id } = findUser.rows[0];
-      const userPassword = await comparePassWord(password, id);
-      if (findUser === false) {
-        return response.status(404).json({
-          status: 'error',
-          message: BAD_EMAIL,
-        });
-      }
-      if (userPassword === false) {
-        return response.status(404).json({
-          status: 'error',
-          message: NO_USER,
-        });
-      }
-      if (userPassword) {
-        const authToken = newToken({ userId: id, email: findUser.rows[0].email });
-        return response.status(200).json({
-          status: LOGIN_SUCCESS,
-          data: {
-            userId: findUser.rows[0].id,
-            token: authToken,
-          },
-        });
-      }
-=======
     const { email, password } = await request.body;
     const findUser = await getOneUserByEmail(email);
     if (findUser === false) {
@@ -42,7 +13,6 @@ const loginUser = async (request, response) => {
         status: 'error',
         message: BAD_EMAIL,
       });
->>>>>>> 75829b867b9f05290c16388e5d85cf5e6c02a289
     }
 
     const { id } = findUser.rows[0];
@@ -63,14 +33,8 @@ const loginUser = async (request, response) => {
         },
       });
     }
-
     conn.end();
   } catch (error) {
-<<<<<<< HEAD
-    console.log('>>>>', error);
-=======
-    // console.log('errr', error)
->>>>>>> 75829b867b9f05290c16388e5d85cf5e6c02a289
     return response.status(500).json({ status: 'error', message: SERVER_ERROR_MESSAGE });
   }
   return false;
