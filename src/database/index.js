a7cfa608e4cd20d0e9351b60c1bf138e1ada185e
teezyfortune <30442140+teezyfortune@users.noreply.github.com>
2019-11-27 16:45:47 +0100

    #comment on gif (#38)
    * ft(comment-gif): employees can comment on gifs
    
    * updates
    
    * ch(updates): fix test:
    
    * (fix-test-error)
    
    * bg(fix-test)
    
    * ch(updates-travis)
    
    * bg(fix--error):
    
    * ch(updates-environment)
    
    * ch(updates-environment)
    
    * ch(updates-environment-variable)
    
    * ft(add-services)
    
    * ft(add-services)

--- src/database/index.js
+++ src/database/index.js
@@ -5,15 +5,13 @@ dotenv.config();
 
 const env = process.env.NODE_ENV || 'development';
 const url = {
-  development: process.env.DATABASE_URL,
-  production: process.env.DATABASE_URL,
+  development: process.env.DATABASE_URL_DEVELOPMENT,
+  production: process.env.DATABASE_URI,
   test: process.env.DATABASE_TEST_URL,
 };
 
 const conn = new Pool({
   connectionString: url[env],
 });
-
-console.log('connection', conn);
 
 export default conn;
